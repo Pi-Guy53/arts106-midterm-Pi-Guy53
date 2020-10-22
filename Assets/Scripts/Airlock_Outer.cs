ffcf89a9b278954c83ea25d11597ed1134392185
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2020-10-22 15:29:39 -0400

    Vaporakis_Mihail Midterm WIP v2

---
+++ Assets/Scripts/Airlock_Outer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airlock_Outer : MonoBehaviour
+{
+    public GameObject DoorObject;
+    public AudioSource audioS;
+    public AudioClip Beep;
+    public AudioClip Opening;
+    public float xAxis = 1f;
+    public float yAxis = 1f;
+    public float zAxis = 1f;
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        DoorObject.transform.position += new Vector3(xAxis, yAxis, zAxis);
+        audioS.PlayOneShot(Opening);
+        audioS.PlayOneShot(Beep);
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+        DoorObject.transform.position -= new Vector3(xAxis, yAxis, zAxis);
+        audioS.PlayOneShot(Opening);
+    }
+}
---
+++ Assets/Scripts/Lift.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lift : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Bottom;
+    public GameObject Top;
+    public int LiftSpeed = 0;
+
+    void Update()
+    {
+        float PlayerDis = Vector3.Distance(transform.position, Player.transform.position);
+        float Up = Vector3.Distance(transform.position, Top.transform.position);
+        float Down = Vector3.Distance(transform.position, Bottom.transform.position);
+
+        transform.position += new Vector3(0, LiftSpeed, 0) * Time.deltaTime *2;
+
+        if (Up < 0.1f)
+        {
+            LiftSpeed = 0;
+        }
+
+        if (Down < 0.1f)
+        {
+            LiftSpeed = 0;
+        }
+
+        if (PlayerDis < 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                if(Up < 0.3f)
+                {
+                    transform.position += new Vector3(0, -.2f, 0);
+                    LiftSpeed = -1;
+                }
+
+                if(Down < 0.3f)
+                {
+                    transform.position += new Vector3(0, .2f, 0);
+                    LiftSpeed = 1;
+                }
+            }
+        }
+    }
+}
