f9fd5c92d5cf35ab9b09e78fc8681226b92d646d
MJV314 <71029215+Pi-Guy53@users.noreply.github.com>
2020-12-12 07:10:10 -0500

    Vaporakis_Mihail Final WIP

---
+++ Assets/UI Screen/MenuUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUI : MonoBehaviour
+{
+    public string MainScene;
+    public string SecondaryScene;
+    public string MenuScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(MainScene);
+    }
+
+    public void OpenSecondaryScene()
+    {
+        SceneManager.LoadScene(SecondaryScene);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(MenuScene);
+    }
+}
---
+++ Assets/UI Screen/PlayerUIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+    public GameObject ExitScreen;
+    public GameObject MainUIScreen;
+    public GameObject Player;
+    public GameObject NewCamera;
+    private bool isHidden = true;
+
+    private void Start()
+    {
+        ExitScreen.SetActive(false);
+        NewCamera.SetActive(false);
+        Player.SetActive(true);
+        MainUIScreen.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isHidden == true)
+            {
+                ExitScreen.SetActive(true);
+                isHidden = false;
+                Player.SetActive(false);
+                NewCamera.SetActive(true);
+                MainUIScreen.SetActive(false);
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cancel();
+            }
+        }
+    }
+
+    public void Cancel()
+    {
+        ExitScreen.SetActive(false);
+        isHidden = true;
+        Player.SetActive(true);
+        NewCamera.SetActive(false);
+        MainUIScreen.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
